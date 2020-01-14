262689fad7eadbc8620fafe548abf7f17ded780b
wangcong <576524838@qq.com>
2020-01-14 23:35:46 +0800

    复习adapter

---
+++ src/main/java/com/example/testwindows/Msg.java
@@ -0,0 +1,19 @@
+package com.example.testwindows;
+
+public class Msg {
+    public static final int TYPE_RECEIVE = 0;
+    public static final int TYPE_SEND = 1;
+    private String content ;
+    private int type;
+    public Msg(String content,int type){
+        this.content = content;
+        this.type = type;
+    }
+    public String getContent(){
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
